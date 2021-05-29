bd03b658b551dddaf5a5eb763b2ea37ef0309d95
e13lau's imac <lauy@pm.me>
2021-05-29 20:20:57 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["DotKit+SnapKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/E13Lau/DotKit.git", from: "0.0.1"),
+        .package(url: "https://github.com/E13Lau/DotKit.git", .upToNextMinor(from: "0.0.1")),
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
     ],
     targets: [
